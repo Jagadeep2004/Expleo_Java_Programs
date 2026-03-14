cb89216de008ecc0897c4aaca71490a578a58340
jagadeep <jagadeep2004@gmail.com>
2026-03-14 22:24:15 +0530

    hands on added

--- ExceptionHandlingHandsOn/Claculator.java
+++ ExceptionHandlingHandsOn/Claculator.java
@@ -1,11 +1,37 @@
 package ExceptionHandlingHandsOn;
-class 
+
+import java.util.Scanner;
 
 public class Claculator {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			try {
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				
+				if (a < 0 || b < 0) {
+					throw new Exception("n or p should not be negative.");
+				}
+				else if (a == 0 && b == 0) {
+					throw new Exception("n and p should not be zero.");
+				}
+				else {
+					int val = 1;
+					for (int j = 0; j < b; j++) {
+						val *= a;
+					}
+					System.out.println(val);
+				}
 
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+		sc.close();
 	}
-
 }
